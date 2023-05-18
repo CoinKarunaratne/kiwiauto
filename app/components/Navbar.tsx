b2be19754e48ecee934deef55d4c26efd0619759
A Karunaratne <coinkarunaratne@gmail.com>
2023-05-18 18:24:37 +1200

    second-commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { ThemeToggle } from "./ThemeToggle";
+import { buttonVariants } from "./ui/button";
+
+function Navbar() {
+  return (
+    <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-[rgb(3,7,17)] z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
+      <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
+        <Link href="/" className={buttonVariants({ variant: "link" })}>
+          Warranty Pass v2.0
+        </Link>
+
+        <div>
+          <ThemeToggle />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@
 import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import animationData from "@/public/114986-ultimate-trading-experience.json";
 import { useRef } from "react";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   const assistantRef = useRef<LottieRefCurrentProps>(null);
   return (
     <main className="flex min-h-screen flex-col md:flex-row items-center justify-between p-24">
+      <Navbar />
       <div className="w-auto">
         <h1 className="text-2xl font-bold">Kiwi Auto Care</h1>
         <p>Official Business App</p>
